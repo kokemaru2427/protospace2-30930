b557afbd0732a18ad7beb809ca148cd37356ca28
kokemaru2427 <kai0405kai0407@icloud.com>
2020-12-13 17:28:43 +0900

    protospaceの修正1

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,10 +1,11 @@
 class PrototypesController < ApplicationController
  
   before_action :authenticate_user!, only: [:create, :edit, :update, :destroy]
+  before_action :move_to_index, except: [:index, :show]
   protect_from_forgery
 
   def index
-    @prototypes = Prototype.all
+    @prototypes = Prototype.includes(:user)
   end
 
   def new
@@ -54,6 +55,12 @@ class PrototypesController < ApplicationController
   private
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 
 end
